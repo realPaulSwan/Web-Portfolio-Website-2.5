65b3c2216c5187e8c466e05029f42629c3cde6ad
realPaulSwan <paul.swan@wsu.edu>
2022-09-20 15:53:07 -0700

    More Detail load Information from Service 1

---
+++ src/app/projects/project-list/project-item/project-details/project-details-data/project-carousel/project-carousel.component.ts
@@ -0,0 +1,88 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Slide } from "./carousel.interface";
+import { trigger, transition, useAnimation } from "@angular/animations";
+
+//https://medium.com/showpad-engineering/angular-animations-lets-create-a-carousel-with-reusable-animations-81c0dd8847e8
+import {
+  AnimationType,
+  scaleIn,
+  scaleOut,
+  fadeIn,
+  fadeOut,
+  flipIn,
+  flipOut,
+  jackIn,
+  jackOut
+} from "./carousel.animations";
+
+@Component({
+  selector: 'app-project-carousel',
+  templateUrl: './project-carousel.component.html',
+  styleUrls: ['./project-carousel.component.scss'],
+  animations: [
+    trigger("slideAnimation", [
+      /* scale */
+      transition("void => scale", [
+        useAnimation(scaleIn, { params: { time: "500ms" } })
+      ]),
+      transition("scale => void", [
+        useAnimation(scaleOut, { params: { time: "500ms" } })
+      ]),
+
+      /* fade */
+      transition("void => fade", [
+        useAnimation(fadeIn, { params: { time: "500ms" } })
+      ]),
+      transition("fade => void", [
+        useAnimation(fadeOut, { params: { time: "500ms" } })
+      ]),
+
+      /* flip */
+      transition("void => flip", [
+        useAnimation(flipIn, { params: { time: "500ms" } })
+      ]),
+      transition("flip => void", [
+        useAnimation(flipOut, { params: { time: "500ms" } })
+      ]),
+
+      /* JackInTheBox */
+      transition("void => jackInTheBox", [
+        useAnimation(jackIn, { params: { time: "700ms" } })
+      ]),
+      transition("jackInTheBox => void", [
+        useAnimation(jackOut, { params: { time: "700ms" } })
+      ])
+    ])
+  ]
+
+})
+export class ProjectCarouselComponent implements OnInit {
+  @Input() slides: Slide[];
+  @Input() animationType = AnimationType.Scale;
+
+  currentSlide = 0;
+
+  constructor() {}
+
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+    console.log("previous clicked, new current slide is: ", this.currentSlide);
+  }
+
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+    console.log("next clicked, new current slide is: ", this.currentSlide);
+  }
+
+  ngOnInit() {
+    this.preloadImages(); // for the demo
+  }
+
+  preloadImages() {
+    for (const slide of this.slides) {
+      new Image().src = slide.src;
+    }
+  }
+}
