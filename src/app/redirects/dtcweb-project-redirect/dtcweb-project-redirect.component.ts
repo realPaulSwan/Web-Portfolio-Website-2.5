b82da7c8e0deabbfbcfe22a08ab365aef88fbdff
realPaulSwan <paul.swan@wsu.edu>
2022-09-26 13:37:47 -0700

    Fixed visual Bugs and improved Contact Page.

---
+++ src/app/redirects/dtcweb-project-redirect/dtcweb-project-redirect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dtcweb-project-redirect',
+  templateUrl: './dtcweb-project-redirect.component.html',
+  styleUrls: ['./dtcweb-project-redirect.component.scss']
+})
+export class DTCWebProjectRedirectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
