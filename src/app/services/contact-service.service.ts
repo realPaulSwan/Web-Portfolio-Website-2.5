c441f76f9afd5c9d461acf8789d0490e6cad186d
realPaulSwan <paul.swan@wsu.edu>
2022-11-14 18:50:09 -0800

    Moved all localhost IP addresses to enviroment variable

--- src/app/services/contact-service.service.ts
+++ src/app/services/contact-service.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 //import {ContactObjectModel} from 'src/app/models/contactObject.model'
+import {environment} from "src/environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {HttpHeaders} from '@angular/common/http';
 
@@ -26,7 +27,7 @@ export class ContactServiceService {
         const data = {name_var: name, feedback_var: feedback}
         //console.log();
         //  const data = ContactObjectModel
-        return this.http.post('http://localhost:5000/api/email/', data,{responseType:'text'}).subscribe(
+        return this.http.post(environment.apiBaseUrl + '/api/email/', data,{responseType:'text'}).subscribe(
             data => {
                 console.log("POST Request is successful ", data);
             },
--- src/app/services/project-detail.service.ts
+++ src/app/services/project-detail.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {BehaviorSubject, Observable} from "rxjs";
 import {Project} from "src/app/models/project.model";
+import {environment} from "../../environments/environment";
 
 
 @Injectable({
@@ -16,7 +17,7 @@ export class ProjectDetailService {
 
   getProjects(dataType: string, localKey: string): Observable<Project[]> {
 
-    return this.http.get<Project[]>("http://localhost:5000/filter/?" + dataType + "=" + localKey);
+    return this.http.get<Project[]>(environment.apiBaseUrl + "/filter/?" + dataType + "=" + localKey);
     //indicate data return with message
 
   }
